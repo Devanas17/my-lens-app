6a27ea4a0fa17cd4167c27b1fca2cfa9b7a8a966
DevAnas17 <anasdeveloper17@gmail.com>
2023-01-16 20:05:45 +0530

    add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import "../styles/globals.css"
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import Header from '../components/Header';
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
     <QueryClientProvider client={queryClient}>
+      <Header /> 
        <Component {...pageProps} />
     </QueryClientProvider>
     </ThirdwebProvider>
